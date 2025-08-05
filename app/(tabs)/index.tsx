697314fd0578757a6240f9fd7685d829e7eb75bc
Johannes Borchard <johannesborchard@gmail.com>
2025-08-05 20:51:05 +0200

    searchbar + header logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { images } from "@/constants/images"
+import SearchBar from "@/shared/ui/SearchBar"
 import { SymbolView } from "expo-symbols"
 import { Image, ScrollView, Text, View } from "react-native"
 import colors from "tailwindcss/colors"
@@ -8,17 +9,20 @@ export default function Index() {
 		<View className="bg-slate-950 flex-1 items-center">
 			<Image source={images.bg} className="absolute w-full z-0" />
 			<ScrollView
-				className="flex-1 px-5 min-h-full pb-10 pt-20"
+				className="flex-1 px-5 min-h-full pb-10 pt-20 w-full"
 				showsVerticalScrollIndicator={false}>
-				<SymbolView
-					name="film"
-					size={50}
-					tintColor={colors.pink[500]}
-					weight="medium"
-					style={{ marginHorizontal: "auto", marginBottom: 10 }}
-				/>
-				{/* <Image source={icons.logo} className="w-12 h-10 mx-auto mb-5 mt-20" /> */}
-				<Text className="text-accent font-bold text-5xl">Welcome!</Text>
+				<View className="flex-row items-center gap-2 w-fit justify-center leading-none mb-5">
+					<SymbolView
+						name="film"
+						size={45}
+						tintColor={colors.purple[400]}
+						weight="medium"
+						/* style={{ marginHorizontal: "auto", marginBottom: 10 }} */
+					/>
+					{/* <Image source={icons.logo} className="w-12 h-10 mx-auto mb-5 mt-20" /> */}
+					<Text className="text-purple-300 font-bold text-4xl">Cinemore</Text>
+				</View>
+				<SearchBar />
 			</ScrollView>
 		</View>
 	)
--- shared/ui/SearchBar.tsx
+++ shared/ui/SearchBar.tsx
@@ -1,10 +1,18 @@
+import { SymbolView } from "expo-symbols"
 import React from "react"
 import { Text, View } from "react-native"
+import colors from "tailwindcss/colors"
 
 const SearchBar = () => {
 	return (
-		<View>
-			<Text>SearchBar</Text>
+		<View className="flex-row items-center bg-slate-900 rounded-full px-5 py-4 w-full">
+			<SymbolView
+				name="magnifyingglass"
+				size={20}
+				tintColor={colors.slate[500]}
+				weight="medium"
+			/>
+			<Text className="text-slate-400 font-medium text-base ml-4">Search</Text>
 		</View>
 	)
 }
