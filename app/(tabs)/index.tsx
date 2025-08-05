ed3935c2578dd8e7874f427bf61215577195b08d
Johannes Borchard <johannesborchard@gmail.com>
2025-08-05 20:14:28 +0200

    add index bg-image + logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,17 @@
-import { Link } from "expo-router"
-import { Text, View } from "react-native"
+import { icons } from "@/constants/icons"
+import { images } from "@/constants/images"
+import { Image, ScrollView, Text, View } from "react-native"
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-			className="bg-slate-950">
-			<Text className="text-accent font-medium text-3xl">Welcome!</Text>
-			<Text className="text-primary text-5xl font-bold">Welcome!</Text>
-			<Link href="/onboarding">Onboarding</Link>
-			<Link href="/movies/avengers">Avengers</Link>
+		<View className="bg-slate-950 flex-1 items-center">
+			<Image source={images.bg} className="absolute w-full z-0" />
+			<ScrollView
+				className="flex-1 px-5 min-h-full pb-10"
+				showsVerticalScrollIndicator={false}>
+				<Image source={icons.logo} className="w-12 h-10 mx-auto mb-5 mt-20" />
+				<Text className="text-accent font-bold text-5xl">Welcome!</Text>
+			</ScrollView>
 		</View>
 	)
 }
