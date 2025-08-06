d91717f586435ce67a5a923aa484aef4f41b9cf5
Johannes Borchard <johannesborchard@gmail.com>
2025-08-06 17:46:05 +0200

    fix poster height

--- features/movies/ui/MovieItem.tsx
+++ features/movies/ui/MovieItem.tsx
@@ -14,7 +14,7 @@ export const MovieCard = memo(function MovieCard({ movie }: { movie: Movie }) {
 							? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
 							: "https://placehold.co/600x400/1a1a1a/ffffff.png",
 					}}
-					className="w-full h-52 rounded-lg"
+					className="w-full h-64 rounded-lg"
 					resizeMode="cover"
 				/>
 				<Text className="text-base font-bold text-slate-200 mt-2">
